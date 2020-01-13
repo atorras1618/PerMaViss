3ee83847a7898f72147478be5c25da5b0ddd03cf
Alvaro Torras Casas <atorras1618@gmail.com>
2020-01-13 12:57:32 +0000

    setup file

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@
 
 from setuptools import setup, find_packages
 
+with open("README.rst", "r") as readme_file:
+    README = readme_file.read()
+
 exec(open("src/permaviss/version.py", "r").read())
 
 setup(
     name="permaviss", 
-    version=__version__
-    url="https://github.com/atorras1618/PerMaViss",
+    version=__version__,
+    description="Persistence Mayer Vietoris spectral sequence for computing Persistent Homology",
+    long_description=README,
+    long_description_content_type="text/x-rst",
+    author_email="atorras1618@gmail.com",
     author="Alvaro Torras Casas",
     licence="MIT",
-    packages=find_packages("src")
+    keywords=["spectral sequence", "persistent homology", "Mayer Vietoris"],
+    packages=find_packages("src"),
+    package_dir={"": "src"},
 )
