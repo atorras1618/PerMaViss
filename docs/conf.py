96402969625a7e02a73551ed23bd651c55f3147a
Alvaro Torras Casas <atorras1618@gmail.com>
2020-01-20 09:12:36 +0000

    fixed problem with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src/permaviss'))
+
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
